e8176a6ce67f914459009f783ef01e3bbee4eabd
leenpaws <s.chhaya@gmail.com>
2016-05-20 11:13:17 -0400

    update

--- Menu.py
+++ Menu.py
@@ -24,57 +24,57 @@ restaurant1 = Restaurant(name="Urban Burger")
 session.add(restaurant1)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+menuItem1 = MenuItem(name="Cheese Pizza", description="Crispy Thin Crust with Zesty Sauce and Fresh Cheese",
                      price="$7.50", course="Entree", restaurant=restaurant1)
-
-session.add(menuItem2)
-session.commit()
-
-menuItem1 = MenuItem(name="French Fries", description="with garlic and parmesan", price="$2.99", course="Appetizer",
-                     restaurant=restaurant1)
 
 session.add(menuItem1)
 session.commit()
 
-menuItem2 = MenuItem(name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
-                     price="$5.50", course="Entree", restaurant=restaurant1)
+menuItem2 = MenuItem(name="French Fries", description="with garlic and parmesan", price="$2.99", course="Appetizer",
+                     restaurant=restaurant1)
 
 session.add(menuItem2)
 session.commit()
 
-menuItem3 = MenuItem(name="Chocolate Cake", description="fresh baked and served with ice cream", price="$3.99",
-                     course="Dessert", restaurant=restaurant1)
+menuItem3 = MenuItem(name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
+                     price="$5.50", course="Entree", restaurant=restaurant1)
 
 session.add(menuItem3)
 session.commit()
 
-menuItem4 = MenuItem(name="Sirloin Burger", description="Made with grade A beef", price="$7.99", course="Entree",
-                     restaurant=restaurant1)
+menuItem4 = MenuItem(name="Chocolate Cake", description="fresh baked and served with ice cream", price="$3.99",
+                     course="Dessert", restaurant=restaurant1)
 
 session.add(menuItem4)
 session.commit()
 
-menuItem5 = MenuItem(name="Root Beer", description="16oz of refreshing goodness", price="$1.99", course="Beverage",
+menuItem5 = MenuItem(name="Sirloin Burger", description="Made with grade A beef", price="$7.99", course="Entree",
                      restaurant=restaurant1)
 
 session.add(menuItem5)
 session.commit()
 
-menuItem6 = MenuItem(name="Iced Tea", description="with Lemon", price="$.99", course="Beverage", restaurant=restaurant1)
+menuItem6 = MenuItem(name="Root Beer", description="16oz of refreshing goodness", price="$1.99", course="Beverage",
+                     restaurant=restaurant1)
 
 session.add(menuItem6)
 session.commit()
 
-menuItem7 = MenuItem(name="Grilled Cheese Sandwich", description="On texas toast with American Cheese", price="$3.49",
-                     course="Entree", restaurant=restaurant1)
+menuItem7 = MenuItem(name="Iced Tea", description="with Lemon", price="$.99", course="Beverage", restaurant=restaurant1)
 
 session.add(menuItem7)
 session.commit()
 
-menuItem8 = MenuItem(name="Veggie Burger", description="Made with freshest of ingredients and home grown spices",
-                     price="$5.99", course="Entree", restaurant=restaurant1)
+menuItem8 = MenuItem(name="Grilled Cheese Sandwich", description="On texas toast with American Cheese", price="$3.49",
+                     course="Entree", restaurant=restaurant1)
 
 session.add(menuItem8)
+session.commit()
+
+menuItem9 = MenuItem(name="Veggie Burger", description="Made with freshest of ingredients and home grown spices",
+                     price="$5.99", course="Entree", restaurant=restaurant1)
+
+session.add(menuItem9)
 session.commit()
 
 # Menu for Super Stir Fry
